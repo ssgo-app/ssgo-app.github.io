883472a48ab8ef3f539213ec0f518682b496376c
Srotiy <22544250+Srotiy@users.noreply.github.com>
2020-07-21 20:04:37 +0800

    first

---
+++ env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'SSGO',
+  // 站点描述
+  description: 'SSGO Panel',
+  // API
+  host: 'https://api-my.ssgo.cloud',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '/bg.jpg'
+}
